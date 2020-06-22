c4baf9b91d98ba6d54b64407890c0254791a61e3
RionC <67234765+RionC@users.noreply.github.com>
2020-06-22 13:42:18 -0600

    Add project files.

---
+++ Interview/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Interview
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name, favColor;
+            int age, drinkingAge = 21;
+
+            Console.Write("What is your name: ");
+            name = Console.ReadLine();
+
+            Console.Write("How old are you " + name + ": ");
+            age = int.Parse(Console.ReadLine());
+
+            Console.Write("What is your favorite color: ");
+            favColor = Console.ReadLine();
+
+            Console.BackgroundColor = (ConsoleColor)Enum.Parse(typeof(ConsoleColor), favColor);
+            Console.Clear();
+
+            Console.WriteLine("Hello " + name + " and Welcome!");
+
+            if (age >= drinkingAge)
+                Console.WriteLine("You should have a beer after work!");
+            else
+                Console.WriteLine("You should have a rootbeer after work!");
+        }
+    }
+}
